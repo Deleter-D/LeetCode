9aa5be7e9a8394c1b089399af5ef0148f0938d23
wangyanpeng04 <wangyanpeng04@MacBook-Pro.local>
2024-07-23 16:31:08 +0800

    update

--- 07_Backtracking/07_93_restore-ip-addresses.cpp
+++ 07_Backtracking/07_93_restore-ip-addresses.cpp
@@ -4,66 +4,60 @@
 #include <iostream>
 #include <string>
 #include <vector>
-using namespace std;
 
-bool isValid(const string &s, int start, int end) {
-    if (start > end)
+std::vector<std::string> res;
+
+bool isValid(std::string &s, int begin, int end) {
+    if (begin > end) {
         return false;
-
-    if (s[start] == '0' && start != end) // 0开头的数字不合法
-        return false;
-
-    int num = 0;
-    for (int i = start; i <= end; i++) {
-        if (s[i] > '9' || s[i] < '0') // 非法字符
-            return false;
-
-        num = num * 10 + (s[i] - '0');
-        if (num > 255) // 数值大于255不合法
-            return false;
     }
-
+    if (s[begin] == '0' && begin != end) {
+        return false;
+    }
+    int num = 0;
+    for (int i = begin; i <= end; i++) {
+        if (s[i] > '9' || s[i] < '0') {
+            return false;
+        }
+        num = num * 10 + (s[i] - '0');
+        if (num > 255) {
+            return false;
+        }
+    }
     return true;
 }
 
-void backtracking(vector<string> &res, string &s, int startIdx, int dotCount) {
-    if (dotCount == 3) { // 点的个数为3时终止
-        // 判断第四段子串是否合法
-        if (isValid(s, startIdx, s.size() - 1)) {
+void backtracking(std::string &s, int startIdx, int pointNum) {
+    if (pointNum == 3) {
+        if (isValid(s, startIdx, s.size() - 1))
             res.push_back(s);
-        }
         return;
     }
 
     for (int i = startIdx; i < s.size(); i++) {
-        if (isValid(s, startIdx, i)) { // 判断[startIdx, i]区间内的子串是否合法
-            s.insert(s.begin() + i + 1, '.'); // 位置i之后插入一个点
-            dotCount++;
-            backtracking(res, s, i + 2,
-                         dotCount); // 插入点之后，下一个起始位置要+2
-            dotCount--;             // 回溯
-            s.erase(s.begin() + i + 1); // 回溯，删除插入的点
+        if (isValid(s, startIdx, i)) {
+            s.insert(s.begin() + i + 1, '.');
+            backtracking(s, i + 2, pointNum + 1);
+            s.erase(s.begin() + i + 1);
         } else {
-            break; // 不合法直接结束循环
+            break;
         }
     }
 }
 
-vector<string> restoreIpAddresses(string s) {
-    vector<string> res;
-    // 剪枝
-    if (s.size() < 4 || s.size() > 12)
-        return res;
-    backtracking(res, s, 0, 0);
-
+std::vector<std::string> restoreIpAddresses(std::string s) {
+    backtracking(s, 0, 0);
     return res;
 }
 
 int main(int argc, char *argv[]) {
-    string s = "25525511135";
-    vector<string> res = restoreIpAddresses(s);
-    for (string str : res) {
-        cout << str << endl;
+    std::string s("25525511135");
+
+    std::vector<std::string> res = restoreIpAddresses(s);
+
+    for (auto str : res) {
+        std::cout << str << std::endl;
     }
+
     return 0;
 }
