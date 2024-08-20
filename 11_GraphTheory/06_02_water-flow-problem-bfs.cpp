8ed225019defb9836ee952d8663d8b42256c47bb
wangyanpeng04 <wangyanpeng04@MacBook-Pro.local>
2024-08-20 11:45:59 +0800

    update

--- 11_GraphTheory/06_02_water-flow-problem-bfs.cpp
+++ 11_GraphTheory/06_02_water-flow-problem-bfs.cpp
@@ -2,28 +2,40 @@
     Topic link: https://kamacoder.com/problempage.php?pid=1175
 */
 #include <iostream>
+#include <queue>
+#include <utility>
 #include <vector>
 
 std::vector<std::vector<int>> dir{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
-void dfs(std::vector<std::vector<int>> &grid,
+void bfs(std::vector<std::vector<int>> &grid,
          std::vector<std::vector<bool>> &visited, int x, int y) {
-    if (visited[x][y])
-        return;
-
+    std::queue<std::pair<int, int>> que;
+    que.push({x, y});
     visited[x][y] = true;
-    for (int i = 0; i < 4; i++) {
-        int x_next = x + dir[i][0];
-        int y_next = y + dir[i][1];
 
-        if (x_next < 0 || x_next >= grid.size() || y_next < 0 ||
-            y_next >= grid[0].size())
-            continue;
+    while (!que.empty()) {
+        auto cur = que.front();
+        que.pop();
 
-        if (grid[x_next][y_next] < grid[x][y])
-            continue;
+        int x_cur = cur.first;
+        int y_cur = cur.second;
 
-        dfs(grid, visited, x_next, y_next);
+        for (int i = 0; i < 4; i++) {
+            int x_next = x_cur + dir[i][0];
+            int y_next = y_cur + dir[i][1];
+
+            if (x_next < 0 || x_next >= grid.size() || y_next < 0 ||
+                y_next >= grid[0].size())
+                continue;
+
+            if (visited[x_next][y_next] ||
+                grid[x_next][y_next] < grid[x_cur][y_cur])
+                continue;
+
+            que.push({x_next, y_next});
+            visited[x_next][y_next] = true;
+        }
     }
 }
 
@@ -43,13 +55,13 @@ int main(int argc, char *argv[]) {
     std::vector<std::vector<bool>> bottomRightVisited(
         n, std::vector<bool>(m, false));
     for (int i = 0; i < n; i++) {
-        dfs(grid, topLeftVisited, i, 0);         // left
-        dfs(grid, bottomRightVisited, i, m - 1); // right
+        bfs(grid, topLeftVisited, i, 0);         // left
+        bfs(grid, bottomRightVisited, i, m - 1); // right
     }
 
     for (int j = 0; j < m; j++) {
-        dfs(grid, topLeftVisited, 0, j); // top
-        dfs(grid, bottomRightVisited, n - 1, j);
+        bfs(grid, topLeftVisited, 0, j); // top
+        bfs(grid, bottomRightVisited, n - 1, j);
     }
 
     for (int i = 0; i < n; i++) {
